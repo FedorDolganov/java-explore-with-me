0a4753d0aca58cf1340aa58da0fe5359e975b097
FedFox <foxyfedor3000@gmail.com>
2026-06-27 13:19:09 +0300

    Working app v1.0.9

--- stats/stats-dto/src/main/java/ru/practicum/statsdto/HitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/statsdto/HitDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.statsdto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
