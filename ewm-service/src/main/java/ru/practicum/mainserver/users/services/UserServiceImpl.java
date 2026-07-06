3d25b87d0685b712fb5a7b4cbfa3f22e71cda9d9
FedFox <158598917+Fed-Fox@users.noreply.github.com>
2026-07-06 12:34:08 +0300

    Working app v2.0.2

--- ewm-service/src/main/java/ru/practicum/mainserver/users/services/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/mainserver/users/services/UserServiceImpl.java
@@ -242,7 +242,7 @@ public class UserServiceImpl implements UserService {
         List<ParticipationRequest> confirmedRequests = new ArrayList<>();
 
         if (eventRequest.getStatus() == UpdateEventRequestStatus.CONFIRMED) {
-            for (int i = 0 ; i < Math.min(freeRequests, requests.size()) ; i++) {
+            for (int i = 0; i < Math.min(freeRequests, requests.size()); i++) {
                 confirmedRequests.add(requests.get(i));
 
                 cancelledRequests.remove(i);
